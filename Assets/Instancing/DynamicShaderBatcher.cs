bcc11e997fcc686915cd9e50c58614a3e65279e9
bennychen <rockerbenny@gmail.com>
2015-09-07 14:24:09 +0800

    Updated DynamicShaderBatcher.cs

--- Assets/Instancing/DynamicShaderBatcher.cs
+++ Assets/Instancing/DynamicShaderBatcher.cs
@@ -12,152 +12,15 @@ using Object = UnityEngine.Object;
 [RequireComponent(typeof(MeshFilter))]
 public class DynamicShaderBatcher : MonoBehaviour
 {
-    #region Constants
-
-    public const int MaxBatchedObject = 9;
-
-    /// <summary>
-    /// The matrix parameter names in the shader
-    /// </summary>
-    public static readonly string[] ModelMatrixParameter = new string[MaxBatchedObject]
-	{ 
-		"ModelMatrix0",
-		"ModelMatrix1",
-		"ModelMatrix2",
-		"ModelMatrix3",
-		"ModelMatrix4",
-		"ModelMatrix5",
-		"ModelMatrix6",
-		"ModelMatrix7",
-		"ModelMatrix8"
-		// Add more if needed, but don't forget to update the shaders :)
-	};
-
-    public static readonly string[] ObjectNumberKeywords = new string[MaxBatchedObject]
-	{
-		"BATCHING_OBJECT_NUMBER_1",
-		"BATCHING_OBJECT_NUMBER_2",
-		"BATCHING_OBJECT_NUMBER_3",
-		"BATCHING_OBJECT_NUMBER_4",
-		"BATCHING_OBJECT_NUMBER_5",
-		"BATCHING_OBJECT_NUMBER_6",
-		"BATCHING_OBJECT_NUMBER_7",
-		"BATCHING_OBJECT_NUMBER_8",
-		"BATCHING_OBJECT_NUMBER_9"
-	};
-
-    #endregion
-    #region Private fields
-
-    /// <summary>
-    /// The mesh list added to be batched
-    /// </summary>
-    [SerializeField]
-    private List<Transform> objectList = new List<Transform>();
-
-    /// <summary>
-    /// The list of the bounds the batch was constructed from. The size of the list should match
-    /// 1-1 with the list of the objects.
-    /// </summary>
-    private List<Bounds> objectBounds = new List<Bounds>();
-
-    /// <summary>
-    /// Cached mesh filter, so we don't use GetComponenet every time
-    /// we need the MeshFilter.
-    /// </summary>
-    protected MeshFilter cachedMeshFilter;
-
-    /// <summary>
-    /// The renderer added to this object to show the batch
-    /// </summary>
-    private Renderer objectRenderer = null;
-
-    /// <summary>
-    /// True if the batch is visible
-    /// </summary>
     public bool Visible { get; private set; }
 
-    #endregion
-    #region Unity methods
-
-    public void Awake()
-    {
-        // Default visibility
-        Visible = false;
-
-        // Get the mesh filter and mesh renderer, or add them if they are not added yet
-        cachedMeshFilter = GetComponent<MeshFilter>();
-        if (cachedMeshFilter == null) cachedMeshFilter = gameObject.AddComponent<MeshFilter>();
-
-        objectRenderer = GetComponent<MeshRenderer>();
-        if (objectRenderer == null) gameObject.AddComponent<MeshRenderer>();
-
-        // Hide the already added objects, this will do nothing if the component is not serialized
-        HideAddedObjects();
-
-        // Batch the meshes if they have not yet been batched
-        if (cachedMeshFilter.sharedMesh == null)
-        {
-            // Build & refresh bounds
-            Batch();
-            List<Transform> transforms = new List<Transform>(objectList);
-            objectList.Clear();
-            foreach (Transform currentTransform in transforms) AddObject(currentTransform);
-        }
-    }
-
-    // These two functions take LODs into account as well! If the object's LOD changes, this will be called.
-    public void OnBecameVisible()
-    {
-        Visible = true;
-        UpdateShaderParameters();
-    }
-    public void OnBecameInvisible() { Visible = false; }
-
-    public void LateUpdate()
-    {
-        if (!Visible) return;
-
-        // The batched objects have probably moved, we don't want them to be frustum culled.
-        // We have to calculate this before the culling occurs.
-        CalculateBoundingBox();
-    }
-
-    public void OnWillRenderObject()
-    {
-        // All batched transforms have been moved during the update. Now we
-        // update the shader parameters
-        UpdateShaderParameters();
-    }
-
-    /// <summary>
-    /// Called when the object is destroy
-    /// </summary>
-    public void OnDestroy()
-    {
-        // We don't need the mesh anymore, so destroy it
-        if (cachedMeshFilter.sharedMesh != null)
-        {
-            Object.DestroyImmediate(cachedMeshFilter.sharedMesh);
-            cachedMeshFilter = null;
-        }
-    }
-
-    #endregion
-    #region Public methods
-
-    /// <summary>
-    /// Adds an object to the list for batching. NOTE: there is a limited number of objects
-    /// that can be added to the batcher (see MaxBatchedObject).
-    /// </summary>
-    /// <param name="newObject">The object to be added</param>
     public void AddObject(Transform newObject)
     {
         // We limit the number of meshes added to a batch, so we accidentally don't
         // make a mistake by adding more meshes than supported
-        if (objectList.Count < MaxBatchedObject)
+        if (_objectList.Count < MaxBatchedObject)
         {
-            objectList.Add(newObject);
+            _objectList.Add(newObject);
 
             Renderer newObjectRenderer = newObject.GetComponent<Renderer>();
             if (newObjectRenderer != null) newObjectRenderer.enabled = false;
@@ -166,37 +29,29 @@ public class DynamicShaderBatcher : MonoBehaviour
             if (filter != null)
             {
                 // If bounds available, then extract them
-                objectBounds.Add(filter.sharedMesh.bounds);
+                _objectBounds.Add(filter.sharedMesh.bounds);
             }
-            else objectBounds.Add(new Bounds());
+            else _objectBounds.Add(new Bounds());
 
             UpdateShaderKeywords();
         }
         else Debug.LogError("Maximum number of batched objects is: " + MaxBatchedObject, this);
     }
 
-    /// <summary>
-    /// Changes the bounds of an object.
-    /// </summary>
-    /// <param name="tranformObject">The object whose bounds to change</param>
-    /// <param name="bounds">The new bounds</param>
     public void ChangeBounds(Transform tranformObject, Bounds bounds)
     {
-        int index = objectList.FindIndex(x => x == tranformObject);
+        int index = _objectList.FindIndex(x => x == tranformObject);
         if (index != -1)
         {
-            objectBounds[index] = bounds;
+            _objectBounds[index] = bounds;
         }
     }
 
-    /// <summary>
-    /// Hides the batched object renderers
-    /// </summary>
     public void HideAddedObjects()
     {
-        for (int i = 0; i < objectList.Count; i++)
+        for (int i = 0; i < _objectList.Count; i++)
         {
-            objectList[i].GetComponent<Renderer>().enabled = false;
+            _objectList[i].GetComponent<Renderer>().enabled = false;
         }
     }
 
@@ -207,12 +62,10 @@ public class DynamicShaderBatcher : MonoBehaviour
     /// </summary>
     public virtual void Batch()
     {
-        // If there are no added mesh filters, nothing to do here.
-        if (objectList.Count == 0) return;
+        if (_objectList.Count == 0) return;
 
-        // Make a mesh list for batching
         List<Mesh> meshList = new List<Mesh>();
-        foreach (Transform currentObject in objectList)
+        foreach (Transform currentObject in _objectList)
         {
             MeshFilter filter = currentObject.GetComponent<MeshFilter>();
 
@@ -220,40 +73,29 @@ public class DynamicShaderBatcher : MonoBehaviour
             else meshList.Add(new Mesh());
         }
 
-        Mesh mesh = cachedMeshFilter.sharedMesh;
-        // If we had a previous batch, destroy it
+        Mesh mesh = _cachedMeshFilter.sharedMesh;
         if (mesh != null) Destroy(mesh);
 
-        // Get new the new batched mesh
-        cachedMeshFilter.sharedMesh = BatchList(meshList);
+        _cachedMeshFilter.sharedMesh = BatchList(meshList);
 
-        // Update material (instantiate a new material) same as objectRenderer.renderer
-        objectRenderer.sharedMaterial = new Material(objectRenderer.sharedMaterial);
+        _objectRenderer.sharedMaterial = new Material(_objectRenderer.sharedMaterial);
         UpdateShaderKeywords();
     }
 
-    /// <summary>
-    /// Updates the keywords on the material
-    /// </summary>
     public void UpdateShaderKeywords()
     {
-        if (objectList.Count == 0) return;
+        if (_objectList.Count == 0) return;
 
-        // First disable all keywords
-        DisableAllKeywords(objectRenderer.sharedMaterial);
+        DisableAllKeywords(_objectRenderer.sharedMaterial);
 
-        // Check if we can do something
-        if (objectList.Count > MaxBatchedObject)
+        if (_objectList.Count > MaxBatchedObject)
         {
             Debug.LogError("Max batched object number exceeded. Update the shader to be able to batch more.", this);
             return;
         }
 
-        // Now set the necessary keywords
-        objectRenderer.sharedMaterial.EnableKeyword(ObjectNumberKeywords[objectList.Count - 1]);
+        _objectRenderer.sharedMaterial.EnableKeyword(ObjectNumberKeywords[_objectList.Count - 1]);
     }
-
-    #region Static
 
     /// <summary>
     /// Static method for batching a list of meshes. Note that the selector information goes to tangent vertex
@@ -272,7 +114,6 @@ public class DynamicShaderBatcher : MonoBehaviour
 
             for (int j = 0; j < mesh.vertexCount; j++)
             {
-                // Add selector data
                 tangentSelectors.Add(new Vector4(i, 0, 0, 0));
             }
 
@@ -291,68 +132,139 @@ public class DynamicShaderBatcher : MonoBehaviour
         return resultMesh;
     }
 
-    #endregion
-    #endregion
-    #region Protected methods
-
-    /// <summary>
-    /// Update the shader transform parameters of the batched object
-    /// </summary>
     protected virtual void UpdateShaderParameters()
     {
-        for (int i = 0; i < objectList.Count; i++)
+        for (int i = 0; i < _objectList.Count; i++)
         {
-            objectRenderer.sharedMaterial.SetMatrix(ModelMatrixParameter[i], objectList[i].transform.localToWorldMatrix);
+            _objectRenderer.sharedMaterial.SetMatrix(ModelMatrixParameter[i], _objectList[i].transform.localToWorldMatrix);
         }
     }
 
-    /// <summary>
-    /// Recalculates the bounding box of the batch
-    /// </summary>
     protected virtual void CalculateBoundingBox()
     {
-        if (cachedMeshFilter.sharedMesh == null)
+        if (_cachedMeshFilter.sharedMesh == null)
         {
             return;
         }
 
-        // Nothing to Calculate
-        if (objectBounds.Count == 0)
+        if (_objectBounds.Count == 0)
         {
-            cachedMeshFilter.sharedMesh.bounds = new Bounds();
+            _cachedMeshFilter.sharedMesh.bounds = new Bounds();
             return;
         }
 
-        // Take the first object for the starting point of the calculation
         Bounds newBounds = new Bounds();
-
-        // Calculate all other objects
-        for (int i = 0; i < objectBounds.Count; i++)
+        for (int i = 0; i < _objectBounds.Count; i++)
         {
-            //newBounds.Encapsulate(new Bounds(transform.InverseTransformPoint(objectList[i].position), objectBounds[i].size));
-            newBounds.Encapsulate(new Bounds(transform.InverseTransformPoint(objectList[i].position), objectBounds[i].size));
+            newBounds.Encapsulate(new Bounds(
+                transform.InverseTransformPoint(_objectList[i].position), _objectBounds[i].size));
         }
 
-        // Set the new bounding box to be used
-        cachedMeshFilter.sharedMesh.bounds = newBounds;
+        _cachedMeshFilter.sharedMesh.bounds = newBounds;
     }
 
-    #endregion
+    private void Awake()
+    {
+        Visible = false;
 
-    /// <summary>
-    /// Disables all keywords on the material.
-    /// </summary>
-    /// <param name="material">The material whose keywords to disable</param>
-    public static void DisableAllKeywords(Material material)
+        _cachedMeshFilter = GetComponent<MeshFilter>();
+        if (_cachedMeshFilter == null) _cachedMeshFilter = gameObject.AddComponent<MeshFilter>();
+
+        _objectRenderer = GetComponent<MeshRenderer>();
+        if (_objectRenderer == null) gameObject.AddComponent<MeshRenderer>();
+
+        HideAddedObjects();
+
+        if (_cachedMeshFilter.sharedMesh == null)
+        {
+            Batch();
+            List<Transform> transforms = new List<Transform>(_objectList);
+            _objectList.Clear();
+            foreach (Transform currentTransform in transforms) AddObject(currentTransform);
+        }
+    }
+
+    // These two functions take LODs into account as well! If the object's LOD changes, this will be called.
+    private void OnBecameVisible()
+    {
+        Visible = true;
+        UpdateShaderParameters();
+    }
+
+    private void OnBecameInvisible() { Visible = false; }
+
+    private void LateUpdate()
+    {
+        if (!Visible) return;
+
+        // The batched objects have probably moved, we don't want them to be frustum culled.
+        // We have to calculate this before the culling occurs.
+        CalculateBoundingBox();
+    }
+
+    private void OnWillRenderObject()
+    {
+        // All batched transforms have been moved during the update. Now we
+        // update the shader parameters
+        UpdateShaderParameters();
+    }
+
+    private void OnDestroy()
+    {
+        // We don't need the mesh anymore, so destroy it
+        if (_cachedMeshFilter.sharedMesh != null)
+        {
+            Object.DestroyImmediate(_cachedMeshFilter.sharedMesh);
+            _cachedMeshFilter = null;
+        }
+    }
+
+    private static void DisableAllKeywords(Material material)
     {
         for (int i = material.shaderKeywords.Length - 1; i >= 0; i--) material.DisableKeyword(material.shaderKeywords[i]);
     }
 
     private void OnDrawGizmos()
     {
-        if (cachedMeshFilter != null)
+        if (_cachedMeshFilter != null)
         {
-            Gizmos.DrawWireCube(cachedMeshFilter.sharedMesh.bounds.center, cachedMeshFilter.sharedMesh.bounds.size);
+            Gizmos.DrawWireCube(_cachedMeshFilter.sharedMesh.bounds.center, _cachedMeshFilter.sharedMesh.bounds.size);
         }
     }
+
+    [SerializeField]
+    private List<Transform> _objectList = new List<Transform>();
+
+    private List<Bounds> _objectBounds = new List<Bounds>();
+    protected MeshFilter _cachedMeshFilter;
+    private Renderer _objectRenderer = null;
+
+    private const int MaxBatchedObject = 9;
+
+    private static readonly string[] ModelMatrixParameter = new string[MaxBatchedObject]
+    { 
+        "ModelMatrix0",
+        "ModelMatrix1",
+        "ModelMatrix2",
+        "ModelMatrix3",
+        "ModelMatrix4",
+        "ModelMatrix5",
+        "ModelMatrix6",
+        "ModelMatrix7",
+        "ModelMatrix8"
+        // Add more if needed, but don't forget to update the shaders :)
+    };
+
+    private static readonly string[] ObjectNumberKeywords = new string[MaxBatchedObject]
+    {
+        "BATCHING_OBJECT_NUMBER_1",
+        "BATCHING_OBJECT_NUMBER_2",
+        "BATCHING_OBJECT_NUMBER_3",
+        "BATCHING_OBJECT_NUMBER_4",
+        "BATCHING_OBJECT_NUMBER_5",
+        "BATCHING_OBJECT_NUMBER_6",
+        "BATCHING_OBJECT_NUMBER_7",
+        "BATCHING_OBJECT_NUMBER_8",
+        "BATCHING_OBJECT_NUMBER_9"
+    };
 }
